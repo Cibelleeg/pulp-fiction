7096a9f69c3c794f8114e1a3489ad9cc9eca40b1
Cibelleeg <cibellegodoylima@gmail.com>
2026-06-21 22:24:17 -0300

    feat: add poster column to produtos table and remove unused columns from usuarios migration

--- prisma/migrations/20260620230945_make_user_fields_optional/migration.sql
+++ prisma/migrations/20260620230945_make_user_fields_optional/migration.sql
@@ -1,9 +1,4 @@
 -- AlterTable
-ALTER TABLE "Filme" ADD COLUMN     "data_fim_cartaz" TIMESTAMP(3),
-ADD COLUMN     "nota" DOUBLE PRECISION,
-ADD COLUMN     "poster" TEXT;
-
--- AlterTable
 ALTER TABLE "usuarios" ALTER COLUMN "nome" DROP NOT NULL,
 ALTER COLUMN "cpf" DROP NOT NULL,
 ALTER COLUMN "telefone" DROP NOT NULL,
